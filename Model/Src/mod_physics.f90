b3c54e1c49c686deda4848f11c868b2449da5073
OtoImai <otim0608@gmail.com>
2026-06-07 15:22:55 +0900

    modified precipitation & evaporation process

--- Model/Src/mod_physics.f90
+++ Model/Src/mod_physics.f90
@@ -2,37 +2,38 @@ module mod_physics
   use mod_io, only: nx, ny, evap, precip
   implicit none
 
+  real, parameter :: rho_water  = 1000.0  ! density of water [kg/m3]
+  real, parameter :: accum_time = 3600.0  ! accumulation time of data [s] (Typically 1 hour = 3600 s for hourly ERA5)
 contains 
 
   subroutine calculate_physics(dt, q, w_model, source_mask)
     real, intent(in)    :: dt 
-    real, intent(inout) :: q(:,:)
-    real, intent(inout) :: w_model(:,:)
+    real, intent(inout) :: q(:,:)           ! colored moisture (tracer)
+    real, intent(inout) :: w_model(:,:)     ! total moisture in the model
     real, intent(in)    :: source_mask(:,:)
 
-    real                :: r
+    real                :: r, e_flux, p_flux
     integer             :: i, j 
 
     do j = 1, ny
       do i = 1, nx 
-        e_rate = ( -evap(i,j) * 1000.0 ) / 3600.0
-        p_rate = ( precip(i,j) * 1000.0 ) / 3600.0
+        ! unit conversion (m/h -> kg/m2/s)
+        e_flux = ( -evap(i,j) * rho_water ) / accum_time
+        p_flux = ( precip(i,j) * rho_water ) / accum_time
 
-        if (e_rate < 0.0) e_rate = 0.0
-        if (p_rate < 0.0) p_rate = 0.0
+        if (e_flux < 0.0) e_flux = 0.0
+        if (p_flux < 0.0) p_flux = 0.0
 
-        if (tcwv(i,j) > 1.0e-3) then
-          tracer_ratio = tracer(i,j) / tcwv(i,j)
-            if (tracer_ratio > 1.0) tracer_ratio = 1.0
-        else
-          tracer_ratio = 0.0
-        endif ! (tcwv(i,j) > 1.0e-3)
+        ! calc the current r 
+        r = q(i,j) / max(w_model(i,j), 1.0e-12)
+        ! water vapor budget
+        w_model(i,j) = w_model(i,j) + dt * (e_flux - p_flux)
+        ! tracer budget
+        q(i,j) = q(i,j) + dt * (e_flux * source_mask(i,j) - p_flux * r)
+
+        if (w_model(i,j) < 0.0) w_model(i,j) = 0.0
+        if (q(i,j) < 0.0) q(i,j) = 0.0
         
-        tracer(i,j) = tracer(i,j) + dt * (e_rate * source_mask(i,j) - p_rate * tracer_ratio)
-
-        if (tracer(i,j) < 0.0) tracer(i,j) = 0.0
-        if (tracer(i,j) > tcwv(i,j)) tracer(i,j) = tcwv(i,j)    
-
       end do ! i
     end do ! j
 
